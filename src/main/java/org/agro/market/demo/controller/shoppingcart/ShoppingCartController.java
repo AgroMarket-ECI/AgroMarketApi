9036597d3a7d27ad58724d18b768b62585d4e351
Johann Bogota <johann.bogota@escuelaing.edu.co>
2021-09-22 22:42:34 -0500

    Access to shopping cart controller

--- src/main/java/org/agro/market/demo/controller/shoppingcart/ShoppingCartController.java
+++ src/main/java/org/agro/market/demo/controller/shoppingcart/ShoppingCartController.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 import org.agro.market.demo.repository.document.Product;
 
+import javax.annotation.security.RolesAllowed;
+
 @RestController
 @RequestMapping( "/v1/shoppingCart" )
 public class ShoppingCartController {
@@ -19,26 +21,30 @@ public class ShoppingCartController {
 	}
 
 	@GetMapping
+	@RolesAllowed("CLIENT")
 	public List<Product> getProductsOfShoppingCart(){
 		//return ShoppingCartService.getProductsOfShoppingCart();
 		return null;
 	}
 	
 	@PutMapping
+	@RolesAllowed("CLIENT")
 	public List<Product> updateShoppingCart(List<Product> products){
 		return userService.updateShoppingCart(products);
 	}
 	
 
-	//@PutMapping("product/{id}")
+	/*@PutMapping("product/{id}")
+	@RolesAllowed("CLIENT")
 	/*public List<User> putProductInShoppingCart (@RequestBody productDto, @PathVariable String id){
 	 	ProductDto productDto = new ProductDto("Fertox Insecticida 300ml", 15.900,"Es para matar insectos en plantas", "Fertox",false,"FertoxInsecticida300ml.jpg");
         Product product = new Product(productDto);
 		return userService.putProductInShoppingCart(product, id);
 	}*/
 
-	//@DeleteMapping("product/{id}")
-	/*public Boolean deleteProductFromShoppingCart (@PathVariable String id){
+	/*@DeleteMapping("product/{id}")
+	@RolesAllowed("CLIENT")
+	public Boolean deleteProductFromShoppingCart (@PathVariable String id){
 		return userService.deleteProductFromShoppingCart(id);
 	}*/
 }
